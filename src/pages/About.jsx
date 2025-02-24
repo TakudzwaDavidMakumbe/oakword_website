bd70a76acaae5bd93e4702d36d7fcde6f8099cce
TakudzwaDavidMakumbe <takuclarity@gmail.com>
2025-02-24 06:01:41 +0200

    Update Navbar logo size and enhance About and Home pages with new content and images

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -9,12 +9,40 @@ import Navbar from '../components/Navbar'
 import CommonCard from '../components/CommonCard'
 import Footer from '../components/Footer'
 import PatternImage from '../components/PatternImage'
+import image from '../../public/oakwood-04949.jpg'
 
 
 const About = () => {
   return (
-    <div className='w-screen h-screen '>
+    <div className="w-screen h-fit">
+      <div className="md:w-screen md:h-full" style={{backgroundImage: `url(${image})`, backgroundSize: 'cover'}}>
         <Navbar/>
+
+        <div className="text-white w-full h-full md:py-5 md:mt-[18rem]">
+        <h1 className="md:text-6xl text-4xl flex justify-center pt-[8rem] text-white md:font-bold">GOING</h1>
+        <h1 className="md:text-6xl flex text-4xl justify-center  text-white md:font-bold">FURTHER IN STYLE</h1>
+        <h1 className="md:text-lg font-bold text-center   text-[#2e388d]  ">A Specialist Supplier and Distributor Of Board Products !!</h1>
+        </div>
+    </div>
+    <div className="w-full md:py-5 md:px-10  h-full">
+         <div className=" w-full h-full ">
+         <h1 className="text-[#00abec] px-3 md:px-0  font-extrabold text-4xl">ABOUT US</h1>
+         </div>
+         <div className="w-full h-full">
+          <p className="text-black py-3 px-3 md:px-0 text-sm md:text-lg md:py-2 ">
+            Oakwood is focused on providing high-end cut and edge solutions to ts customers. Our range of services integrate design, research and innovation to provide seamless innovation cut and edge solutions including the latest high end cabinet hardware, wardrobe storage solutions, accessories and hardware.
+          </p>
+          <p className="text-black py-3 px-3 md:px-0 text-sm md:text-lg md:py-2">
+            We offer the best cut and edge solutions with well experienced designers and technicians on hand to assit our diverse clients to put their dreams to life. Our clients define their dreams and we action them into reality. We have a fully equipped factory with intelligent manufacturing equipment and technology, enabling us to provide precise, seamless products. 
+          </p>
+          <p className="text-black px-3 md:px-0 text-sm md:mb-2 md:py-2 md:text-lg">
+            Oakwood is commiEed to quality to service. We create value for customers by providing them with design and ancillary services at no cost. We are here to help you realize your dream!
+          </p>
+          <div className="bg-[#00abec] py-1 w-[40%] px-5 ml-5 md:ml-0 mt-3 md:mt-0 md:px-0  md:w-[15%]">
+            
+          </div>
+         </div>
+        </div>
     </div>
   )
   
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,17 +16,28 @@ import HumanIntelligence from '../components/home/HumanIntelligence'
 import Footer from '../components/Footer'
 import CommonCard from '../components/CommonCard'
 import Pillars from '../components/home/Pillars'
+import  image1 from "../../public/1.png"
+import  image2 from "../../public/2.png"
+import  image3 from "../../public/3.png"
+import  image4 from "../../public/4.png"
+import  back from "../../public/back.jpg"
+import  image5 from "../../public/5.png"
+import  image6 from "../../public/6.png"
+import  image7 from "../../public/7.png" 
+import  Logo from "../../public/logo_2.png" 
+
 const Home = () => {
   return (
+    <div className='w-full h-full'>
    <div className="md:w-screen md:h-full" style={{backgroundImage: `url(${bg})`, backgroundSize: 'cover'}}>
     <Navbar />
 
-    <section className="w-full h-screen" >
+    <section className="w-full md:h-screen" >
       <h1 className="md:text-7xl text-4xl flex justify-center pt-[8rem] text-white md:font-bold">GOING</h1>
       <h1 className="md:text-7xl flex text-4xl justify-center  text-white md:font-bold">FURTHER IN STYLE</h1>
       <h1 className="md:text-7xl flex justify-center font-thin text-4xl text-white"> OAKWOOD</h1>
       <section className="md:w-[100%] pt-[10rem]  md:pt-[14rem] px-2 md:px-[10rem] h-full">
-      <div className=" flex-1  md:flex md:py-8 md:rounded-3xl px-2 md:px-0    bg-amber-600 ">
+      <div className=" flex-1  md:flex md:py-8 md:rounded-3xl px-2 md:px-0    bg-[#f28020] ">
        <div className="flex-col md:px-4 md:w-[65%]">
        <h1 className="font-extrabold text-xl   text-white md:text-3xl">Need A QUOTE?</h1>
        <p className="text-white text-sm py-2 md:py-0">Our range of services integrate design,research and innovation to provide seamless innovative cut and edge solutions</p>
@@ -35,11 +46,131 @@ const Home = () => {
         <input type="email" placeholder='Enter email address here' className='text-white border w-full  rounded-lg px-3 py-2' name="" id="" />
        </div>
       </div>
-
     </section>
     </section>
+   </div>
 
-   
+   <section className="w-full flex md:py-28   h-full">
+   <div className="flex-col w-full md:px-[18rem] justify-center items-center h-full">
+     <h1 className="font-extrabold md:text-4xl text-center text-[#2e388d] ">WHY US?</h1>
+     <p className="text-black md:text-2xl">
+      We pride ourselves on the close relationships we develop with our clients. It`s through nurturing relationships that we learn how to truly understand their needs and how best to serve them.
+     </p>
+   </div>
+ </section>
+
+ <div className="w-full md:pb-10 md:px-20 h-full">
+ <div className="w-full md:flex justify-center h-full">
+    <div className="flex-col justify-center text-center md:px-12 border-r">
+     <div className="flex justify-center text-center">
+     <img src={image1} alt="" className="md:w-[50%] " />
+     </div>
+      <h1 className="text-[#12b3ca] font-bold">INTEGRITY</h1>
+      <p className="text-black text-sm w-[100%] mt-1 ">We hold ourselves to the highest standards and behave honestly and openly</p>
+    </div>
+    <div className="flex-col justify-center text-center md:px-12 border-r">
+      <div className="flex justify-center text-center">
+      <img src={image2} alt="" className="md:w-[50%]" />
+      </div>
+      <h1 className="text-[#12b3ca] font-bold">EXCELLENCE</h1>
+      <p className="text-black text-sm w-[100%] mt-1 ">We develop high quality products to meet your precise needs.</p>
+    </div>
+    <div className="flex-col justify-center text-center md:px-12 border-r">
+     <div className="flex justify-center text-center">
+     <img src={image3} alt="" className="md:w-[50%]" />
+     </div>
+     <h1 className="text-[#12b3ca] font-bold">INNOVATION</h1>
+      <p className="text-black text-sm w-[100%] mt-1 ">We think creatively, act boldly and action our dreams into reality.</p>
+    </div>
+    <div className="flex-col justify-center text-center md:px-12 ">
+     <div className="flex justify-center text-center">
+     <img src={image4} alt="" className="md:w-[40%]" />
+     </div>
+     <h1 className="text-[#12b3ca] font-bold">RESULTS</h1>
+      <p className="text-black text-sm w-[100%] mt-1 ">We stand by our high quality products and commit ourselves to producing durable products.</p>
+    </div>
+   </div>
+ </div>
+
+ <div className="w-full h-full">
+  <img src={back} alt="" className="w-[100%]" />
+ </div>
+
+ <div className="w-full md:mt-5 h-full">
+ <h1 className="font-extrabold md:text-4xl text-center text-[#2e388d] ">TESTIMONIALS</h1>
+ <h1 className="font-normal md:text-2xl text-center text-[#2e388d] ">What Clients Say About Us</h1>
+
+ <div className="md:flex justify-center md:px-10 md:py-16  ">
+ <div className="md:px-5 md:w-[100%] justify-center items-center ">
+ <div className="bg-[#12b2c9] md:py-10  text-white rounded-lg">
+    <p className="text-sm md:px-10">
+      Oakwood exceeded our expectations! Their attention to detail, professionalism and dedication to delivering exceptional results made our kitchen renovation a dream came true
+    </p>
+    <div className="flex md:ml-32  md:absolute py">
+      <img src={image5} alt="" className="md:w-[20%] border-gray-400 border rounded-full" />
+    </div>
+  </div>
+  
+ </div>
+
+ <div className="md:px-5 md:w-[100%] justify-center items-center ">
+ <div className="bg-[#12b2c9] md:py-10  text-white rounded-lg">
+    <p className="text-sm md:px-10 md:py-2">
+      We were blown away by the quality of Oakwood`s work! Their expertise in board cutting, kitchen design and cabinet making is unparalleled.
+    </p>
+    <div className="flex md:ml-32 md:absolute md:py-2">
+      <img src={image6} alt="" className="md:w-[20%] border-gray-400 border rounded-full" />
+    </div>
+  </div>
+  
+ </div>
+
+ <div className="md:px-5 md:w-[100%] justify-center items-center ">
+ <div className="bg-[#12b2c9] md:py-10  text-white rounded-lg">
+    <p className="text-sm md:px-10 md:py-2">
+      Oakwood made our renovation process seamless and stress-free. Their team was responsive, knowledgeable and passionate about delivering outstanding results.
+    </p>
+    <div className="flex md:ml-32 md:absolute py">
+      <img src={image7} alt="" className="md:w-[30%] border-gray-400 border rounded-full" />
+    </div>
+  </div>
+  
+ </div>
+
+
+ </div>
+ </div>
+
+ <div className="w-full mb-2 mt-[4rem] md:px-16 h-full md:flex">
+  <div className="md:flex md:mr-16 w-[30%]">
+    <img src={Logo} alt="" className="md:w-[70%]" />
+  </div>
+  <div className="md:flex-col">
+  <h1 className="font-extrabold md:text-2xl  text-[#2e388d] ">Contact Us</h1>
+  <div className="font-bold">
+  <p className="text-black py-1">
+    NRZ Complex, Seke Road, Harare Zimbabwe
+  </p>
+  <p className="text-black py-1">
+  +263772734257|+263775572191
+  </p>
+  <p className="text-black">
+  (0242)2332573/4
+  </p>
+  </div>
+  </div>
+ </div>
+
+ <div className="w-full h-full px-5 mb-0 py-6 flex justify-between items-center bg-[#f28020] ">
+  <div className="text-white">
+    <h1 className="">Oakwood; All Rights Reserved</h1>
+  </div>
+  <div className="text-white">
+    DesignedBy Hyve Agency
+  </div>
+ </div>
+
+ 
    </div>
   )
 }
